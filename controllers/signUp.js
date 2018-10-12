fbd88ca9977af985e01187c10d12aba17394fd9e
Ashwanth A R <ashwanth1109@gmail.com>
2018-10-12 23:39:50 +0530

    added sign up route

---
+++ controllers/signUp.js
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------------
+// SET DEPENDENCIES
+//------------------------------------------------------------------------------------
+const express = require("express");
+const signUp = express.Router();
+// Get user model
+
+signUp.get("/", (req, res) => {
+  res.render("signUp/index.ejs");
+});
+
+module.exports = signUp;
