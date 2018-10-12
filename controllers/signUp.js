affb5e8278ce0a1e6c3bc98d9756cf0139380c31
Ashwanth A R <ashwanth1109@gmail.com>
2018-10-13 00:40:16 +0530

    minor

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -3,6 +3,11 @@
 //------------------------------------------------------------------------------------
 const express = require("express");
 const signUp = express.Router();
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+const db = mongoose.connection;
 // Get user model
 
 signUp.get("/", (req, res) => {
