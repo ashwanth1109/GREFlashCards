8689abada511cf06bd123043a4088ad0f0cb0c3f
Ashwanth A R <ashwanth1109@gmail.com>
2018-10-16 20:43:03 +0530

    finished all routes

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,6 +5,9 @@ const deck1 = require("../data/deck1");
 // Get word model
 const { User, Word } = require("../models/users");
 
+//------------------------------------------------------------------------------------
+// EMPTY HOME PAGE ROUTE
+//------------------------------------------------------------------------------------
 dashboard.get("/", (req, res) => {
   const { user } = req.session;
   res.render("app/dashboard.ejs", {
@@ -14,26 +17,30 @@ dashboard.get("/", (req, res) => {
     word: {
       name: "You are yet to add a word",
       definition: "You are yet to add a word",
-      difficulty: 4
+      difficulty: 4,
+      notes: "You are yet to add notes for this word"
     },
     home: true
   });
 });
 
+//------------------------------------------------------------------------------------
+// INDEX + SHOW ROUTE
+//------------------------------------------------------------------------------------
 dashboard.get("/easy/:id", (req, res) => {
-  console.log(`entering dashboard 0 element get req`);
+  // console.log(`entering dashboard 0 element get req`);
   const { user } = req.session;
   const { id } = req.params;
-  console.log(`logging user`);
-  console.log(user);
+  // console.log(`logging user`);
+  // console.log(user);
 
   User.findById({ _id: user._id }, (err, user) => {
     if (req.session.user) {
       Word.findById({ _id: user.easyWords[id] }, (err, word) => {
-        console.log(`logging word`);
-        console.log(word);
+        // console.log(`logging word`);
+        // console.log(word);
         if (err) {
-          console.log("error");
+          // console.log("error");
           res.send(err);
         } else {
           res.render("app/dashboard.ejs", {
@@ -59,6 +66,9 @@ dashboard.get("/easy/:id", (req, res) => {
   });
 });
 
+//------------------------------------------------------------------------------------
+// DESTORY/DELETE ROUTE
+//------------------------------------------------------------------------------------
 dashboard.delete("/easy/:id", (req, res) => {
   const { id } = req.params;
   const { user } = req.session;
@@ -92,6 +102,80 @@ dashboard.delete("/easy/:id", (req, res) => {
           // res.redirect("/dashboard/easy/0");
         }
       });
+    });
+  }
+});
+
+//------------------------------------------------------------------------------------
+// EDIT ROUTE
+//------------------------------------------------------------------------------------
+dashboard.get("/:id/edit", (req, res) => {
+  const { id } = req.params;
+  if (req.session.user) {
+    Word.findById({ _id: req.session.user.easyWords[id] }, (err, word) => {
+      if (err) {
+        res.send(err);
+      } else {
+        console.log(`printing word in edit route`);
+        console.log(word);
+        res.render("app/edit.ejs", {
+          user: req.session.user,
+          word: word,
+          id: id
+        });
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+//------------------------------------------------------------------------------------
+// UPDATE ROUTE
+//------------------------------------------------------------------------------------
+dashboard.put("/:id", (req, res) => {
+  console.log(`entering put route`);
+  const { body } = req;
+  const { user } = req.session;
+  const { id } = req.params;
+
+  let difficulty;
+  switch (body.difficulty) {
+    case "Easy":
+      body.difficulty = 1;
+      break;
+    case "Medium":
+      body.difficulty = 2;
+      break;
+    case "Hard":
+      body.difficulty = 3;
+      break;
+    default:
+      body.difficulty = 3;
+  }
+
+  console.log(body);
+  if (user) {
+    User.findById({ _id: user._id }, (err, user) => {
+      if (err) {
+        res.send(`Error: ${err}`);
+      } else {
+        console.log("in update route finding word");
+        console.log(user.easyWords[id]);
+        Word.findByIdAndUpdate(
+          { _id: user.easyWords[id] },
+          body,
+          (err, updatedWord) => {
+            if (err) {
+              res.send(`Error ${err}`);
+            } else {
+              console.log(`updated word is`);
+              console.log(updatedWord);
+              res.redirect("/dashboard/easy/0");
+            }
+          }
+        );
+      }
     });
   }
 });
