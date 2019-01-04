cbadad002e1b16585b2c62597e1cacd80f0ffe04
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-05 04:34:23 +0530

    BUGFIX: Add words can now take any category and grammarly ghost issue resolved for textarea

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -18,288 +18,286 @@ const { User, Word } = require("../models/users");
 // I GOT THIS WORD DOWN ROUTE
 //------------------------------------------------------------------------------------
 dashboard.get("/changeFamiliarity/:id/:urlId", (req, res) => {
-  const { id } = req.params;
-  const { urlId } = req.params;
-  Word.findById({ _id: id }, (err, word) => {
-    if (err) {
-      console.log(`Error: ${err}`);
-      res.send(err);
-    } else {
-      if (word.familiarity !== 5) {
-        word.familiarity++;
-        Word.findByIdAndUpdate(
-          { _id: word.id },
-          { familiarity: word.familiarity },
-          (err, updatedWord) => {
-            if (err) {
-              console.log(`Error: err`);
-              res.send(err);
+    const { id } = req.params;
+    const { urlId } = req.params;
+    Word.findById({ _id: id }, (err, word) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            res.send(err);
+        } else {
+            if (word.familiarity !== 5) {
+                word.familiarity++;
+                Word.findByIdAndUpdate(
+                    { _id: word.id },
+                    { familiarity: word.familiarity },
+                    (err, updatedWord) => {
+                        if (err) {
+                            console.log(`Error: err`);
+                            res.send(err);
+                        } else {
+                            console.log(`Updated Word is ${updatedWord}`);
+                            res.redirect(`/dashboard/easy/${urlId}`);
+                        }
+                    }
+                );
             } else {
-              console.log(`Updated Word is ${updatedWord}`);
-              res.redirect(`/dashboard/easy/${urlId}`);
+                res.redirect(`/dashboard/easy/${urlId}`);
             }
-          }
-        );
-      } else {
-        res.redirect(`/dashboard/easy/${urlId}`);
-      }
-    }
-  });
+        }
+    });
 });
 
 //------------------------------------------------------------------------------------
 // EMPTY HOME PAGE ROUTE
 //------------------------------------------------------------------------------------
 dashboard.get("/", (req, res) => {
-  const { user } = req.session;
-  res.render("app/dashboard.ejs", {
-    user: user,
-    id: "-1",
-    words: [],
-    word: {
-      name: "You are yet to add a word",
-      definition: "You are yet to add a word",
-      difficulty: 4,
-      notes: "You are yet to add notes for this word"
-    },
-    home: true
-  });
+    const { user } = req.session;
+    res.render("app/dashboard.ejs", {
+        user: user,
+        id: "-1",
+        words: [],
+        word: {
+            name: "You are yet to add a word",
+            definition: "You are yet to add a word",
+            difficulty: 4,
+            notes: "You are yet to add notes for this word"
+        },
+        home: true
+    });
 });
 
 //------------------------------------------------------------------------------------
 // INDEX + SHOW ROUTE
 //------------------------------------------------------------------------------------
 dashboard.get("/easy/:id", (req, res) => {
-  const { id } = req.params;
-  const { user } = req.session;
+    const { id } = req.params;
+    const { user } = req.session;
 
-  if (user) {
-    User.findById({ _id: user._id }, (err, user) => {
-      Word.find({ _id: { $in: user.easyWords } }, (err, words) => {
-        if (err) {
-          res.send(err);
-        } else {
-          console.log(`========================================`);
-          console.log(words);
-          console.log(`========================================`);
-          Word.findById({ _id: user.easyWords[id] }, (err, word) => {
-            if (err) {
-              res.send(err);
-            } else {
-              res.render("app/dashboard.ejs", {
-                user: user,
-                id: id,
-                words: words,
-                word: word,
-                home: false
-              });
-            }
-          });
-        }
-      });
-    });
-  } else {
-    res.redirect("/");
-  }
+    if (user) {
+        User.findById({ _id: user._id }, (err, user) => {
+            Word.find({ _id: { $in: user.easyWords } }, (err, words) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    console.log(`========================================`);
+                    console.log(words);
+                    console.log(`========================================`);
+                    Word.findById({ _id: user.easyWords[id] }, (err, word) => {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            res.render("app/dashboard.ejs", {
+                                user: user,
+                                id: id,
+                                words: words,
+                                word: word,
+                                home: false
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
 //------------------------------------------------------------------------------------
 // NEW ROUTE
 //------------------------------------------------------------------------------------
 dashboard.get("/new", (req, res) => {
-  if (req.session.user) {
-    res.render("app/new.ejs", {
-      user: req.session.user
-    });
-  } else {
-    res.redirect("/");
-  }
+    if (req.session.user) {
+        res.render("app/new.ejs", {
+            user: req.session.user
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
 //------------------------------------------------------------------------------------
 // CREATE ROUTE
 //------------------------------------------------------------------------------------
 dashboard.post("/", (req, res) => {
-  const { body } = req;
-  const { _id } = req.session.user;
-  let difficulty;
-  switch (body.difficulty) {
-    case "Easy":
-      difficulty = 1;
-      break;
-    case "Medium":
-      difficulty = 2;
-      break;
-    case "Hard":
-      difficulty = 3;
-      break;
-    default:
-      difficulty = 3;
-  }
-  const homeWordId = req.session.user.easyWords[0];
-
-  const word = new Word({
-    userId: _id,
-    name: body.name,
-    definition: body.definition,
-    notes: body.notes,
-    difficulty: difficulty,
-    familiarity: 1
-  });
-
-  word.save((err, word) => {
-    if (err) {
-      console.log(err);
+    const { body } = req;
+    const { _id } = req.session.user;
+    let difficulty;
+    switch (body.difficulty) {
+        case "Easy":
+            difficulty = 1;
+            break;
+        case "Medium":
+            difficulty = 2;
+            break;
+        case "Hard":
+            difficulty = 3;
+            break;
+        default:
+            difficulty = 3;
     }
-    if (word.difficulty === 1) {
-      User.findByIdAndUpdate(
-        { _id: _id },
-        { $push: { easyWords: word } },
-        { new: true },
-        (err, updatedUser) => {
-          if (err) {
-            res.send(err);
-          } else {
-            console.log(updatedUser);
-            res.redirect(`/dashboard/easy/0`);
-          }
+    const homeWordId = req.session.user.easyWords[0];
+
+    const word = new Word({
+        userId: _id,
+        name: body.name,
+        definition: body.definition,
+        notes: body.notes,
+        difficulty: difficulty,
+        familiarity: 1
+    });
+
+    word.save((err, word) => {
+        if (err) {
+            console.log(err);
         }
-      );
-    }
-  });
+        User.findByIdAndUpdate(
+            { _id: _id },
+            { $push: { easyWords: word } },
+            { new: true },
+            (err, updatedUser) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    console.log(updatedUser);
+                    res.redirect(`/dashboard/easy/0`);
+                }
+            }
+        );
+    });
 });
 
 //------------------------------------------------------------------------------------
 // DESTORY/DELETE ROUTE
 //------------------------------------------------------------------------------------
 dashboard.delete("/easy/:id", (req, res) => {
-  const { id } = req.params;
-  const { user } = req.session;
-  console.log("entering delete route");
-  if (req.session.user) {
-    User.findById({ _id: user._id }, (err, user) => {
-      Word.findOneAndRemove({ _id: user.easyWords[id] }, (err, word) => {
-        if (err) {
-          res.send(err);
-        } else {
-          user.easyWords.splice(id, 1);
-          User.findByIdAndUpdate(
-            { _id: user._id },
-            { easyWords: user.easyWords },
-            { new: true },
-            (err, updatedUser) => {
-              if (err) {
-                res.send(err);
-              } else {
-                console.log(id);
-                if (user.easyWords.length < 1) {
-                  console.log(`redirecting to dashboard`);
-                  res.redirect("/dashboard");
+    const { id } = req.params;
+    const { user } = req.session;
+    console.log("entering delete route");
+    if (req.session.user) {
+        User.findById({ _id: user._id }, (err, user) => {
+            Word.findOneAndRemove({ _id: user.easyWords[id] }, (err, word) => {
+                if (err) {
+                    res.send(err);
                 } else {
-                  console.log("redirecting to 1st element");
-                  res.redirect("/dashboard/easy/0");
+                    user.easyWords.splice(id, 1);
+                    User.findByIdAndUpdate(
+                        { _id: user._id },
+                        { easyWords: user.easyWords },
+                        { new: true },
+                        (err, updatedUser) => {
+                            if (err) {
+                                res.send(err);
+                            } else {
+                                console.log(id);
+                                if (user.easyWords.length < 1) {
+                                    console.log(`redirecting to dashboard`);
+                                    res.redirect("/dashboard");
+                                } else {
+                                    console.log("redirecting to 1st element");
+                                    res.redirect("/dashboard/easy/0");
+                                }
+                            }
+                        }
+                    );
                 }
-              }
-            }
-          );
-        }
-      });
-    });
-  }
+            });
+        });
+    }
 });
 
 //------------------------------------------------------------------------------------
 // EDIT ROUTE
 //------------------------------------------------------------------------------------
 dashboard.get("/:id/edit", (req, res) => {
-  console.log("edit route reached");
-  const { id } = req.params;
-  const { user } = req.session;
-  if (user) {
-    User.findById({ _id: user._id }, (err, user) => {
-      if (err) {
-        console.log(`entering error in user`);
-        res.send(err);
-      } else {
-        Word.findById({ _id: user.easyWords[id] }, (err, word) => {
-          if (err) {
-            res.send(err);
-          } else {
-            console.log(`printing word in edit route`);
-            console.log(word);
-            res.render("app/edit.ejs", {
-              user: user,
-              word: word,
-              id: id
-            });
-          }
+    console.log("edit route reached");
+    const { id } = req.params;
+    const { user } = req.session;
+    if (user) {
+        User.findById({ _id: user._id }, (err, user) => {
+            if (err) {
+                console.log(`entering error in user`);
+                res.send(err);
+            } else {
+                Word.findById({ _id: user.easyWords[id] }, (err, word) => {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        console.log(`printing word in edit route`);
+                        console.log(word);
+                        res.render("app/edit.ejs", {
+                            user: user,
+                            word: word,
+                            id: id
+                        });
+                    }
+                });
+            }
         });
-      }
-    });
-  } else {
-    res.redirect("/");
-  }
+    } else {
+        res.redirect("/");
+    }
 });
 
 //------------------------------------------------------------------------------------
 // UPDATE ROUTE
 //------------------------------------------------------------------------------------
 dashboard.put("/:id", (req, res) => {
-  console.log(`entering put route`);
-  const { body } = req;
-  const { user } = req.session;
-  const { id } = req.params;
+    console.log(`entering put route`);
+    const { body } = req;
+    const { user } = req.session;
+    const { id } = req.params;
 
-  let difficulty;
-  switch (body.difficulty) {
-    case "Easy":
-      body.difficulty = 1;
-      break;
-    case "Medium":
-      body.difficulty = 2;
-      break;
-    case "Hard":
-      body.difficulty = 3;
-      break;
-    default:
-      body.difficulty = 3;
-  }
+    let difficulty;
+    switch (body.difficulty) {
+        case "Easy":
+            body.difficulty = 1;
+            break;
+        case "Medium":
+            body.difficulty = 2;
+            break;
+        case "Hard":
+            body.difficulty = 3;
+            break;
+        default:
+            body.difficulty = 3;
+    }
 
-  console.log(body);
+    console.log(body);
 
-  if (user) {
-    User.findById({ _id: user._id }, (err, user) => {
-      if (err) {
-        res.send(`Error: ${err}`);
-      } else {
-        const word = {
-          _id: user.easyWords[id],
-          userId: user._id,
-          name: body.name,
-          definition: body.definition,
-          notes: body.notes,
-          difficulty: body.difficulty
-        };
-        console.log(`word is `);
-        console.log(word);
-        // console.log("in update route finding word");
-        // console.log(user.easyWords[id]);
-        Word.findByIdAndUpdate(
-          { _id: user.easyWords[id] },
-          word,
-          (err, updatedWord) => {
+    if (user) {
+        User.findById({ _id: user._id }, (err, user) => {
             if (err) {
-              res.send(`Error ${err}`);
+                res.send(`Error: ${err}`);
             } else {
-              console.log(`updated word is`);
-              console.log(updatedWord);
-              res.redirect("/dashboard/easy/0");
+                const word = {
+                    _id: user.easyWords[id],
+                    userId: user._id,
+                    name: body.name,
+                    definition: body.definition,
+                    notes: body.notes,
+                    difficulty: body.difficulty
+                };
+                console.log(`word is `);
+                console.log(word);
+                // console.log("in update route finding word");
+                // console.log(user.easyWords[id]);
+                Word.findByIdAndUpdate(
+                    { _id: user.easyWords[id] },
+                    word,
+                    (err, updatedWord) => {
+                        if (err) {
+                            res.send(`Error ${err}`);
+                        } else {
+                            console.log(`updated word is`);
+                            console.log(updatedWord);
+                            res.redirect("/dashboard/easy/0");
+                        }
+                    }
+                );
             }
-          }
-        );
-      }
-    });
-  }
+        });
+    }
 });
 
 module.exports = dashboard;
--- public/app.js
+++ public/app.js
@@ -1,105 +1,102 @@
 $(() => {
-  //------------------------------------------------------------------------------------
-  // CARD FLIP ANIMATION EXPORTED IN THROUGH EXTERNAL LIBRARY
-  //------------------------------------------------------------------------------------
-  $(".card").flip({ axis: "x" });
+    //------------------------------------------------------------------------------------
+    // CARD FLIP ANIMATION EXPORTED IN THROUGH EXTERNAL LIBRARY
+    //------------------------------------------------------------------------------------
+    $(".card").flip({ axis: "x" });
 
-  //------------------------------------------------------------------------------------
-  // NEED TO FIGURE OUT WHAT I WROTE THIS FOR
-  //------------------------------------------------------------------------------------
-  let difficulty = "";
-  $(`.difficulty`).on("click", e => {
-    if ($(e.target).hasClass("yellow")) {
-      $(e.target).removeClass("yellow");
-      difficulty = "";
-      $("#wordDifficulty")
-        .attr("name", "difficulty")
-        .attr("value", difficulty);
-    } else {
-      difficulty = $(e.target).attr("difficulty");
-      $("#wordDifficulty")
-        .attr("name", "difficulty")
-        .attr("value", difficulty);
-      $(".difficulty").removeClass("yellow");
-      $(e.target).addClass("yellow");
+    let difficulty = "";
+    $(`.difficulty`).on("click", e => {
+        if ($(e.target).hasClass("yellow")) {
+            $(e.target).removeClass("yellow");
+            difficulty = "";
+            $("#wordDifficulty")
+                .attr("name", "difficulty")
+                .attr("value", difficulty);
+        } else {
+            difficulty = $(e.target).attr("difficulty");
+            $("#wordDifficulty")
+                .attr("name", "difficulty")
+                .attr("value", difficulty);
+            $(".difficulty").removeClass("yellow");
+            $(e.target).addClass("yellow");
+        }
+    });
+
+    if ($(".previous").hasClass("grey")) {
+        $(".previous").removeClass("grey");
     }
-  });
 
-  if ($(".previous").hasClass("grey")) {
-    $(".previous").removeClass("grey");
-  }
+    const currentURL = $(location).attr("href");
+    const urlStringArray = currentURL.split("/");
 
-  const currentURL = $(location).attr("href");
-  const urlStringArray = currentURL.split("/");
+    const nextId = Number(urlStringArray[urlStringArray.length - 1]) + 1;
+    const previousId = Number(urlStringArray[urlStringArray.length - 1]) - 1;
+    urlStringArray[urlStringArray.length - 1] = (
+        Number(urlStringArray[urlStringArray.length - 1]) + 1
+    ).toString();
 
-  const nextId = Number(urlStringArray[urlStringArray.length - 1]) + 1;
-  const previousId = Number(urlStringArray[urlStringArray.length - 1]) - 1;
-  urlStringArray[urlStringArray.length - 1] = (
-    Number(urlStringArray[urlStringArray.length - 1]) + 1
-  ).toString();
+    $(".previous").on("click", () => {
+        urlStringArray[urlStringArray.length - 1] = previousId;
+        const newURL = urlStringArray.join("/");
+        window.location.href = newURL;
+    });
+    $(".next").on("click", () => {
+        urlStringArray[urlStringArray.length - 1] = nextId;
+        const newURL = urlStringArray.join("/");
+        window.location.href = newURL;
+    });
 
-  $(".previous").on("click", () => {
-    urlStringArray[urlStringArray.length - 1] = previousId;
-    const newURL = urlStringArray.join("/");
-    window.location.href = newURL;
-  });
-  $(".next").on("click", () => {
-    urlStringArray[urlStringArray.length - 1] = nextId;
-    const newURL = urlStringArray.join("/");
-    window.location.href = newURL;
-  });
-
-  if (previousId < 0) {
-    $(".previous").off("click");
-    $(".previous")
-      .removeClass("yellow")
-      .removeClass("hoverButton")
-      .addClass("grey");
-  }
-
-  if (nextId === Number($("#wordPosition").attr("totalWords"))) {
-    $(".next").off("click");
-    $(".next")
-      .removeClass("yellow")
-      .removeClass("hoverButton")
-      .addClass("grey");
-  }
-  console.log($("#wordPosition").attr("currentWord"));
-  console.log($("#wordPosition").attr("totalWords"));
-
-  $(".disabled")
-    .removeClass("yellow hoverButton")
-    .addClass("grey")
-    .off("click");
-
-  const disabledButtons = $(".disabled");
-  for (const disabledButton of disabledButtons) {
-    console.log(
-      $(disabledButton).hasClass("gotWordDown") &&
-        $(disabledButton).hasClass("delete")
-    );
-    if ($(disabledButton).hasClass("delete")) {
-      console.log(`detecting disabled delete button`);
-      console.log($(".delete"));
-      $(".delete").attr("disabled", "disabled");
+    if (previousId < 0) {
+        $(".previous").off("click");
+        $(".previous")
+            .removeClass("yellow")
+            .removeClass("hoverButton")
+            .addClass("grey");
     }
-    if ($(disabledButton).hasClass("gotWordDown")) {
-      console.log(`entering got word downs if statement`);
-      console.log($(disabledButton).children());
-      $(disabledButton)
-        .children()
-        .click(function(e) {
-          e.preventDefault();
-        });
+
+    if (nextId === Number($("#wordPosition").attr("totalWords"))) {
+        $(".next").off("click");
+        $(".next")
+            .removeClass("yellow")
+            .removeClass("hoverButton")
+            .addClass("grey");
     }
-  }
+    console.log($("#wordPosition").attr("currentWord"));
+    console.log($("#wordPosition").attr("totalWords"));
 
-  $(".indexButton").addClass("hoverButton");
-  $(".indexButton").on("click", () => {
-    const wordId = $(event.target).attr("wordId");
-    console.log(`Word Id is ${wordId}`);
-    window.location.href("../../easy/0");
-  });
+    $(".disabled")
+        .removeClass("yellow hoverButton")
+        .addClass("grey")
+        .off("click");
 
-  // if (nextId > )
+    const disabledButtons = $(".disabled");
+    for (const disabledButton of disabledButtons) {
+        console.log(
+            $(disabledButton).hasClass("gotWordDown") &&
+                $(disabledButton).hasClass("delete")
+        );
+        if ($(disabledButton).hasClass("delete")) {
+            console.log(`detecting disabled delete button`);
+            console.log($(".delete"));
+            $(".delete").attr("disabled", "disabled");
+        }
+        if ($(disabledButton).hasClass("gotWordDown")) {
+            console.log(`entering got word downs if statement`);
+            console.log($(disabledButton).children());
+            $(disabledButton)
+                .children()
+                .click(function(e) {
+                    e.preventDefault();
+                });
+        }
+    }
+
+    $(".indexButton").addClass("hoverButton");
+    $(".indexButton").on("click", () => {
+        const wordId = $(event.target).attr("wordId");
+        console.log(`Word Id is ${wordId}`);
+        window.location.href("../../easy/0");
+    });
+
+    // if (nextId > )
 });
